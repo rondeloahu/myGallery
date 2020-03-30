ccf2a45114f67c6ec9e2dfdbf640ce9d0e5ca2a8
Rondeliahu <62349879+rondeloahu@users.noreply.github.com>
2020-03-30 18:38:25 +0300

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,55 @@
+function renderProjects() {
+  var projects = getProjects()
+  var strHtmls = projects.map(function getProjectHTML(project) {
+    return `
+        <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal" onclick="renderModal('${project.id}')">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid" src="img/proj/0${project.id}.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${project.title}</h4>
+          <p class="text-muted">${project.desc}</p>
+        </div>
+      </div>
+        `
+  })
+  document.querySelector('.prot').innerHTML = strHtmls.join('')
+}
+
+function renderModal(projectId) {
+  var selectedProject = getProjectById(projectId);
+  var strHtml = `
+                <h2>${selectedProject.name}</h2>
+                <p class="item-intro text-muted">${selectedProject.title}</p>
+                <img class="img-fluid d-block mx-auto" src="img/proj/0${selectedProject.id}.jpg" alt="">
+                <p>${selectedProject.desc}</p>
+                <ul class="list-inline">
+                  <li>Date: ${selectedProject.date}</li>
+                  <li>Client: Coding Acedemy School</li>
+                  <li>Category: ${selectedProject.labels}</li>
+                </ul>
+                <button onclick="getToProj('${selectedProject.url}')" class="btn btn-primary">
+                Try Project
+                </button>
+                <button class="btn btn-secondry" data-dismiss="modal" type="button">
+                <i class="fa fa-times"></i>
+                Close Project
+                </button>`
+  document.querySelector('.modal-body').innerHTML = strHtml;
+}
+function onSendMail() {
+  const SUBJECT = document.querySelector('input[name="subject"]').value;
+  const BODY = document.querySelector('textarea[name="text"]').value;
+  window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=gal31010@gmail.com&su=${SUBJECT}&body=${BODY}`, '_blank');
+}
+
+function getToProj(url) {
+  window.open(url, '_blank');
+}
+
+renderProjects();
---
+++ js/main.js
@@ -0,0 +1,60 @@
+console.log('Starting up');
+// global vars
+var gProjects = [
+    {
+        id: '1',
+        name: "Book Store",
+        title: "Easy to use - online book store",
+        desc: "A way to sell your books online",
+        url: `/proj/1/index.html`,
+        publishedAt: Date.now(),
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: '2',
+        name: "Mine Sweeper",
+        title: "Minesweeper game",
+        desc: "Best way to burn some time on",
+        url: `/proj/2/index.html`,
+        publishedAt: Date.now(),
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: '3',
+        name: "Picture Pick",
+        title: "Pick the right Pic",
+        desc: "Fun game to play with",
+        url: `/proj/3/index.html`,
+        publishedAt: Date.now(),
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: '4',
+        name: "Touch The Num",
+        title: "Touch the spesific number",
+        desc: "go test your skills in touch the number game",
+        url: `/proj/4/index.html`,
+        publishedAt: Date.now(),
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: '5',
+        name: "Pop the Ballon",
+        title: "Popin ballons is fun",
+        desc: "Can you pop ballon? and what about three? go ahead!",
+        url: `/proj/5/index.html`,
+        publishedAt: Date.now(),
+        labels: ["Matrixes", "keyboard events"],
+    }
+];
+function getProjects() {
+    return gProjects;
+}
+
+function getProjectById(projectId) {
+    var project = gProjects.find(proj => proj.id === projectId);
+    console.log(project);
+    return project;
+
+
+}
